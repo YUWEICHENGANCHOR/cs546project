7c6f77b32b8fd7b68cbc33569c7ed335a24e2b39
bobo <>
2017-11-30 13:37:58 -0500

    add restauants and reviews part framework

--- hobokenRest/data/index.js
+++ hobokenRest/data/index.js
@@ -1,11 +1,11 @@
 const restaurantsData=require("./restaurants");
 const reviewsData=require("./reviews");
-const usersData=require("./users");
+//const usersData=require("./users");
 //const profilesData=require("./profiles");
 
 module.exports={
     restaurants:restaurantsData,
-    reviews:reviewsData,
-    users:usersData,
+    reviews:reviewsData
+    //users:usersData,
     //profiles:profilesData
 };
---
+++ hobokenRest/routes/restaurants.js
@@ -0,0 +1,86 @@
+const router=require("express").Router();
+const data=require("../data");
+const restaurantsData=data.restaurants;
+
+router.get("/", async (req,res)=>{
+    try{
+        const getData=await restaurantsData.getAllRestaurants();
+        res.json(getData);
+    }catch(e){
+        res.status(500).json({error: e});
+    }   
+});
+
+router.get("/:id", async (req,res)=>{
+    try{
+        const getData=await restaurantsData.getRestaurantById(req.params.id);
+        res.json(getData);
+    }catch(e){
+        console.log(e);
+        res.status(404).json({error:"The restaurant not found."});
+    }
+});
+
+router.post("/", async (req,res)=>{
+    let restaurantInfo=req.body; 
+    if (!restaurantInfo) {
+        res.status(400).json({ error: "You must provide data to create a restaurant." });
+        return;
+    }
+    if (!restaurantInfo.R_cuisine) {
+        res.status(400).json({ error: "You must provide a cuisine." }); 
+        return;
+    }
+    if (!restaurantInfo.R_name) {
+        res.status(400).json({ error: "You must provide name." });
+        return;
+    }
+    if (!restaurantInfo.R_href) {
+        res.status(400).json({ error: "You must provide href." });
+        return;
+    }
+    if (!restaurantInfo.R_location) {
+        res.status(400).json({ error: "You must provide location." });
+        return;
+    }
+    try{
+        const newRestaurant=await restaurantsData.addRestaurant(restaurantInfo.R_cuisine,restaurantInfo.R_name,restaurantInfo.R_href,restaurantInfo.R_location);
+        res.json(newRestaurant);
+    }catch(e){
+        console.log(e);
+        res.sendStatus(500);
+    } 
+});
+
+router.put("/:id", async (req,res)=>{
+    let restaurantInfo=req.body;
+    try{
+        await restaurantsData.getRestaurantById(req.params.id);
+    }catch(e){
+        res.status(404).json({ error: "Restaurant not found" });
+    }
+    try{
+        const result=await restaurantsData.updateRestaurant(req.params.id,restaurantInfo);
+        res.json(result);
+    }catch(e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+router.delete("/:id",async (req,res)=>{
+   // const restaurantInfo=await restaurantsData.getRestaurantById(req.params.id);
+    try{
+        try{
+            const result=await restaurantsData.deleteRestaurant(req.params.id);
+            res.sendStatus(200);
+        }catch(e){
+            res.sendStatus(500);
+        }
+    }catch(e){
+        console.log(e);
+        res.status(404).json({ error: "restaurant not found" });
+    }
+});
+
+module.exports=router;
